529a0c610f7280fe936dd83d2b3438f24278e4e3
Prasanna Thilakarathne <prasanna.srilal.7@gmail.com>
2020-09-18 02:32:50 +0530

    create food project with controller servlet

---
+++ src/com/prasanna/foodcart/FoodCart.java
@@ -0,0 +1,24 @@
+package com.prasanna.foodcart;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FoodCart")
+public class FoodCart extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//get the data from db(model)
+		
+		//redirect to a different page(view)
+		RequestDispatcher dispatcher=  request.getRequestDispatcher("Show-Food.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
